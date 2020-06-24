9d03d6ada8303500fbb2f88186b0016342182206
trygve <trygve.woldseth@gmail.com>
2020-06-24 09:24:12 +0200

    added download file template

--- ServerApi/src/main/webapp/WEB-INF/classes/DownloadFileServlet.java
+++ ServerApi/src/main/webapp/WEB-INF/classes/DownloadFileServlet.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DownloadFileServlet extends HttpServlet {
 
+
+
     @Override
     public void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
